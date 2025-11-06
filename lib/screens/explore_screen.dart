72db86eb14d17d7403a327019326642da39fc8ca
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-05 19:42:27 -0500

    Update explorer screen

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:taller_final/screens/movie_detail_screen.dart';
+import 'package:taller_final/widgets/date_selector.dart';
+import 'package:taller_final/widgets/movie_card.dart';
 
 class ExploreScreen extends StatelessWidget {
   const ExploreScreen({super.key});
@@ -22,6 +25,50 @@ class ExploreScreen extends StatelessWidget {
                   borderSide: BorderSide.none,
                 ),
               ),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              'Explorer',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              'Top Movies',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 20),
+            const DateSelector(),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 300,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  MovieCard(
+                    title: 'The irishman',
+                    imageUrl: '',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const MovieDetailScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(width: 20),
+                  MovieCard(
+                    title: 'Her',
+                    imageUrl:
+                        'https://tse3.mm.bing.net/th/id/OIP.TJ4J-terls0z3jqy8amShQHaJ_?rs=1&pid=ImgDetMain&o=7&rm=3',
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Trailers',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
           ],
         ),
